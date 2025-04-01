e4a617351cd29fe2e37e309e6f97c2ff0c3f7e74
ooyniz <ooyniz@gmail.com>
2025-04-01 18:33:29 +0900

    Initial commit

---
+++ src/main/java/com/catch_lotto/CatchLottoApplication.java
@@ -0,0 +1,13 @@
+package com.catch_lotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchLottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchLottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catch_lotto/CatchLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catch_lotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchLottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
